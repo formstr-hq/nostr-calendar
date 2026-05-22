f6c1048c7d9ef6448b90ca0f4cc6ff2e0e3aefbc
Ix2 <16651792+geralt-debugs@users.noreply.github.com>
2026-05-22 22:58:38 +0200

    Change waitForAll option to false in publish function (#170)

--- src/components/CalendarEventEdit.tsx
+++ src/components/CalendarEventEdit.tsx
@@ -483,7 +483,7 @@ export function CalendarEventEdit({
               eventToSave,
               {
                 onRelayComplete,
-                waitForAll: true
+                waitForAll: false
               }
             );
           setSignedEventForRetry(calendarEvent);
