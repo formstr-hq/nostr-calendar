49df887213bf74d888739e7b35c463bf01cfbda3
rama <16651792+geralt-debugs@users.noreply.github.com>
2026-06-18 23:57:47 +0200

    updated logging

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./main.css";
 import { initCalendarFavicon } from "./utils/calendarFavicon";
+import { overrideConsole } from "./utils/logger";
 
+overrideConsole();
 initCalendarFavicon();
 
 createRoot(document.getElementById("root")!).render(<App />);
--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -8,6 +8,10 @@ interface Logger {
   error: LogMethod;
 }
 
+const IS_PROD = import.meta.env.PROD;
+
+const noop: LogMethod = () => {};
+
 function timestamp(): string {
   return new Date().toLocaleTimeString(undefined, {
     hour: "2-digit",
@@ -23,11 +27,33 @@ export function createLogger(tag: string): Logger {
     (...args) =>
       method(`${timestamp()} [${tag}]`, ...args);
 
+  const warnMethod = wrap(console.warn.bind(console));
+  const errorMethod = wrap(console.error.bind(console));
+
+  if (IS_PROD) {
+    return {
+      log: noop,
+      info: noop,
+      warn: warnMethod,
+      debug: noop,
+      error: errorMethod,
+    };
+  }
+
   return {
     log: wrap(console.log.bind(console)),
     info: wrap(console.info.bind(console)),
-    warn: wrap(console.warn.bind(console)),
+    warn: warnMethod,
     debug: wrap(console.debug.bind(console)),
-    error: wrap(console.error.bind(console)),
+    error: errorMethod,
   };
+}
+
+export function overrideConsole(): void {
+  const logger = createLogger("CONSOLE");
+  console.log = logger.log;
+  console.info = logger.info;
+  console.warn = logger.warn;
+  console.debug = logger.debug;
+  console.error = logger.error;
 }
