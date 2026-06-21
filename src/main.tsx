2516f319414224ddc657d16159be97f41b8e2294
rama <16651792+geralt-debugs@users.noreply.github.com>
2026-06-21 12:06:08 +0200

    added pwa

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,15 @@ import App from "./App";
 import "./main.css";
 import { initCalendarFavicon } from "./utils/calendarFavicon";
 import { overrideConsole } from "./utils/logger";
+import { isNative } from "./utils/platform";
 
 overrideConsole();
 initCalendarFavicon();
+
+if (!isNative) {
+  import("virtual:pwa-register").then(({ registerSW }) => {
+    registerSW({ immediate: true });
+  });
+}
 
 createRoot(document.getElementById("root")!).render(<App />);
